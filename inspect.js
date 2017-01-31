70c6eae55a7e723fbe659a2e8ab1a45283214180
Ryan Alyn Porter <rap@endymion.com>
2017-01-31 18:25:37 -0500

    One way to do it, though asynchronous recursion.

--- inspect.js
+++ inspect.js
@@ -7,6 +7,8 @@ const w3cjs = require('w3cjs');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
+var failure = false;
+
 // Read in urls from website.yml and set urlList variable/array accordingly
 try {
   // var websiteList = yaml.safeLoad(fs.readFileSync('website.yml', 'utf8'));     // yaml file with only 1 url
@@ -17,120 +19,135 @@ try {
   console.log(e);
 }
 
-// Loop over urlList and run each url through scrape() and validateHTML()
-for (var url in urlList) {   
-  scrape(urlList[url].toString());  
-  validateHTML(urlList[url].toString());
-}
+// Send the urlList to functions that will use
+// asynchrounous recursion to process them.
+inspect(urlList);
+// validateHTML(urlList);
 
-function scrape(url) {
-  jsdom.env({
-    url: url,
-    scripts: ['http://code.jquery.com/jquery.js'],
-    done: function (err, window) {
-      
-      // if (err) {
-      //   console.log(err);
-      //   return;
-      // }
+function inspect(urlList) {
 
-      console.log("Scraping " + url + "...");
+  // Clone the array because we're going to damage it.
+  // https://davidwalsh.name/javascript-clone-array
+  var inspectURLList = urlList.slice(0);
 
-      // A non-zero length page title is required.
-      assert(window.$('title').length === 1,
-        'There is exactly 1 page title');
-      assert(window.document.title.length > 0,
-        'Page title is greater than 1 character');
-      console.log('<title> : ', window.document.title);
+  var inspectURL = function() {
 
-      // A non-zero length meta description is required.
-      var description = window.$('meta[name="description"]').attr('content');
-      assert((description.length > 0),
-        'Page includes a <meta name="description" tag with content greater than 1 character');
-      assert(((description.length > 150) && (description.length < 160)),
-        'Meta description content should ideally be between 150 and 160 characters');
-      console.log('<meta name="description" content => ',
-        window.$('meta[name="description"]').attr('content'));
-      console.log(description.length + ' characters');
+    if (!inspectURLList.length) endWithStatusCode();
 
-      // Multiple favicons are required.
-      var favicons = window.$('link[rel="icon"]');
-      assert(favicons.length >= 1, 
-        'Page has more than 1 favicon');
-      favicons.each(function(i) {
-        console.log((window.$('link[rel="icon"]')[i].getAttribute('sizes')));
-      });
+    var url = inspectURLList.shift().toString();
 
-      // Multiple apple-touch-icons are required.
-      var appleicons = window.$('link[rel="apple-touch-icon"]');
-      assert(appleicons.length >= 1, 
-        'Page has more than 1 apple-touch-icon');
-      appleicons.each(function(i) {
-        console.log((window.$('link[rel="apple-touch-icon"]')[i].getAttribute('sizes')));
-      });
+    jsdom.env({
+      url: url,
+      scripts: ['http://code.jquery.com/jquery.js'],
+      done: function (err, window) {
 
-      // Twitter card tags required.
-      console.log("Checking for Twitter Card tags...");
-      assert(window.$('meta[name="twitter:card"]').attr('content').length >= 1,
-        'Page has twitter:card content');
-      console.log(window.$('meta[name="twitter:card"]').attr('content'));
-      assert(window.$('meta[name="twitter:site"]').attr('content').length >= 1,
-        'Page has twitter:site content');
-      console.log(window.$('meta[name="twitter:site"]').attr('content'));
-      assert(window.$('meta[name="twitter:creator"]').attr('content').length >= 1,
-        'Page has twitter:creator content');
-      console.log(window.$('meta[name="twitter:creator"]').attr('content'));
-      assert(window.$('meta[name="twitter:title"]').attr('content').length >= 1,
-        'Page has twitter:title content');
-      console.log(window.$('meta[name="twitter:title"]').attr('content'));
-      assert(window.$('meta[name="twitter:description"]').attr('content').length >= 1,
-        'Page has twitter:description content');
-      console.log(window.$('meta[name="twitter:description"]').attr('content'));
-      assert(window.$('meta[name="twitter:image"]').attr('content').length >= 1,
-        'Page has twitter:image content');
-      console.log(window.$('meta[name="twitter:image"]').attr('content'));
+        if (err) {
+          console.log(err);
+          return;
+        }
 
-      // Facebook Open Graph tags required.
-      console.log("Checking for Facebook Open Graph tags...");
-      assert(window.$('meta[property="og:url"]').attr('content').length >= 1,
-        'Page has og:url content');
-      console.log(window.$('meta[property="og:url"]').attr('content'));
-      assert(window.$('meta[property="og:type"]').attr('content').length >= 1,
-        'Page has og:type content');
-      console.log(window.$('meta[property="og:type"]').attr('content'));
-      assert(window.$('meta[property="og:title"]').attr('content').length >= 1,
-        'Page has og:title content');
-      console.log(window.$('meta[property="og:title"]').attr('content'));
-      assert(window.$('meta[property="og:description"]').attr('content').length >= 1,
-        'Page has og:description content');
-      console.log(window.$('meta[property="og:description"]').attr('content'));
-      assert(window.$('meta[property="og:image"]').attr('content').length >= 1,
-        'Page has og:image content');
-      console.log(window.$('meta[property="og:image"]').attr('content'));
+        console.log("Inspecting " + url + "...");
 
-      // Google Tag Manager required.
-      console.log("Checking for Google Tag Manager script tags...");
-      var gtmScript = window.$('head > script:contains("//www.googletagmanager.com/gtm.js?id=")');
-      assert(gtmScript.length === 1,
-        'Page has a Google Tag Manager <script> in the <head>');
-      console.log(gtmScript.html());
-      var gtmNoscript = window.$('body > noscript:contains("//www.googletagmanager.com/ns.html?id=")');
-      assert(gtmNoscript.length === 1,
-        'Page has a Google Tag Manager <noscript> in the <body>');
-      console.log(gtmNoscript.html());
-      
-      // Marketo form required
-      // console.log("Checking for Marketo form...");
-      // var marketoForm = window.$('body > script[src="//app-ab06.marketo.com/js/forms2/js/forms2.min.js"]');
-      // assert(marketoForm.length === 1,
-      //   'Page has a Google Tag Manager <noscript> in the <body>');
-      // console.log(marketoForm);
+        // A non-zero length page title is required.
+        assert(window.$('title').length === 1,
+          'There is exactly 1 page title');
+        assert(window.document.title.length > 0,
+          'Page title is greater than 1 character');
+        console.log('<title> : ', window.document.title);
 
-      // Log Assertions vs. Passed stats
-      stats();
+        // A non-zero length meta description is required.
+        var description = window.$('meta[name="description"]').attr('content');
+        assert((description.length > 0),
+          'Page includes a <meta name="description" tag with content greater than 1 character');
+        assert(((description.length > 150) && (description.length < 160)),
+          'Meta description content should ideally be between 150 and 160 characters');
+        console.log('<meta name="description" content => ',
+          window.$('meta[name="description"]').attr('content'));
+        console.log(description.length + ' characters');
 
-    }
-  });
+        // Multiple favicons are required.
+        var favicons = window.$('link[rel="icon"]');
+        assert(favicons.length >= 1,
+          'Page has more than 1 favicon');
+        favicons.each(function(i) {
+          console.log((window.$('link[rel="icon"]')[i].getAttribute('sizes')));
+        });
+
+        // Multiple apple-touch-icons are required.
+        var appleicons = window.$('link[rel="apple-touch-icon"]');
+        assert(appleicons.length >= 1,
+          'Page has more than 1 apple-touch-icon');
+        appleicons.each(function(i) {
+          console.log((window.$('link[rel="apple-touch-icon"]')[i].getAttribute('sizes')));
+        });
+
+        // Twitter card tags required.
+        console.log("Checking for Twitter Card tags...");
+        assert(window.$('meta[name="twitter:card"]').attr('content').length >= 1,
+          'Page has twitter:card content');
+        console.log(window.$('meta[name="twitter:card"]').attr('content'));
+        assert(window.$('meta[name="twitter:site"]').attr('content').length >= 1,
+          'Page has twitter:site content');
+        console.log(window.$('meta[name="twitter:site"]').attr('content'));
+        assert(window.$('meta[name="twitter:creator"]').attr('content').length >= 1,
+          'Page has twitter:creator content');
+        console.log(window.$('meta[name="twitter:creator"]').attr('content'));
+        assert(window.$('meta[name="twitter:title"]').attr('content').length >= 1,
+          'Page has twitter:title content');
+        console.log(window.$('meta[name="twitter:title"]').attr('content'));
+        assert(window.$('meta[name="twitter:description"]').attr('content').length >= 1,
+          'Page has twitter:description content');
+        console.log(window.$('meta[name="twitter:description"]').attr('content'));
+        assert(window.$('meta[name="twitter:image"]').attr('content').length >= 1,
+          'Page has twitter:image content');
+        console.log(window.$('meta[name="twitter:image"]').attr('content'));
+
+        // Facebook Open Graph tags required.
+        console.log("Checking for Facebook Open Graph tags...");
+        assert(window.$('meta[property="og:url"]').attr('content').length >= 1,
+          'Page has og:url content');
+        console.log(window.$('meta[property="og:url"]').attr('content'));
+        assert(window.$('meta[property="og:type"]').attr('content').length >= 1,
+          'Page has og:type content');
+        console.log(window.$('meta[property="og:type"]').attr('content'));
+        assert(window.$('meta[property="og:title"]').attr('content').length >= 1,
+          'Page has og:title content');
+        console.log(window.$('meta[property="og:title"]').attr('content'));
+        assert(window.$('meta[property="og:description"]').attr('content').length >= 1,
+          'Page has og:description content');
+        console.log(window.$('meta[property="og:description"]').attr('content'));
+        assert(window.$('meta[property="og:image"]').attr('content').length >= 1,
+          'Page has og:image content');
+        console.log(window.$('meta[property="og:image"]').attr('content'));
+
+        // Google Tag Manager required.
+        console.log("Checking for Google Tag Manager script tags...");
+        var gtmScript = window.$('head > script:contains("//www.googletagmanager.com/gtm.js?id=")');
+        assert(gtmScript.length === 1,
+          'Page has a Google Tag Manager <script> in the <head>');
+        console.log(gtmScript.html());
+        var gtmNoscript = window.$('body > noscript:contains("//www.googletagmanager.com/ns.html?id=")');
+        assert(gtmNoscript.length === 1,
+          'Page has a Google Tag Manager <noscript> in the <body>');
+        console.log(gtmNoscript.html());
+
+        // Marketo form required
+        // console.log("Checking for Marketo form...");
+        // var marketoForm = window.$('body > script[src="//app-ab06.marketo.com/js/forms2/js/forms2.min.js"]');
+        // assert(marketoForm.length === 1,
+        //   'Page has a Google Tag Manager <noscript> in the <body>');
+        // console.log(marketoForm);
+
+        // Log Assertions vs. Passed stats
+        stats();
+
+        // Inspect the next URL.
+        inspectURL();
+      }
+    });
+  }
+
+  inspectURL();
 }
 
 function validateHTML(url) {
@@ -182,6 +199,16 @@ function stats() {
     } else {
         console.log(colors.red('Assertions: ' + assertions));
         console.log(colors.red('Passed:     ' + passed));
-        process.exit(1);
+        failure = true;
     }
+}
+
+function endWithStatusCode() {
+  if (failure == true) {
+    console.log(colors.red('INSPECTION FAILED'));
+    process.exit(1);
+  } else {
+    console.log(colors.green('INSPECTION PASSED'));
+    process.exit();
+  }
 }
